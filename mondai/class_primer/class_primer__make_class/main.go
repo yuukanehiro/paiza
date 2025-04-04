d6a56e7ea0006506b2fba08d5a03bce14d917068
yuu kanehiro <yuu.kanehiro@zeroboard.jp>
2025-04-04 15:24:49 +0900

    クラス・構造体メニュー | 5問目 done

--- mondai/class_primer/class_primer__make_class/main.go
+++ mondai/class_primer/class_primer__make_class/main.go
@@ -10,7 +10,41 @@ import (
 
 var sc = bufio.NewScanner(os.Stdin)
 
+type Employee struct {
+	Number int
+	Name   string
+}
+
 func main() {
+	// 入力を受け取る
+	commandCount := nextLineWithoutEmptySpace("int").([]int)[0]
+
+	employees := map[int]Employee{}
+	employeeCount := 0
+	for i := 0; i < commandCount; i++ {
+		command := nextLineWithoutEmptySpace("string").([]string)
+		if command[0] == "make" {
+			employeeCount++
+			number, _ := strconv.Atoi(command[1])
+			name := command[2]
+			employees[employeeCount] = Employee{Number: number, Name: name}
+		} else if command[0] == "getnum" {
+			registerID := atoi(command[1])
+			fmt.Println(employees[registerID].Number)
+		} else if command[0] == "getname" {
+			registerID := atoi(command[1])
+			fmt.Println(employees[registerID].Name)
+		}
+	}
+}
+
+func atoi(s string) int {
+	i, e := strconv.Atoi(s)
+	if e != nil {
+		panic(e)
+	}
+
+	return i
 }
 
 // 行を取得してstringで返却
@@ -19,16 +53,12 @@ func nextLine() string {
 	return sc.Text()
 }
 
-// 行を取得してinterface{}で返却
-// 注意: interface{}で返却されるので、キャストを忘れないこと
-// 利用例
-// ・array := nextLineBySeparator(" ", "string").([]string) // []stringで取得
-// ・array := nextLineBySeparator(" ", "int").([]int)　// []intで取得
-func nextLineBySeparator(separator string, elementType string) interface{} {
+// 空白を除いた行を取得してinterface{}で返却
+func nextLineWithoutEmptySpace(elementType string) interface{} {
 	line := nextLine()
 
 	var numberArray []string
-	numberArray = strings.Split(line, separator)
+	numberArray = strings.Fields(line) // 空白を除去して分割
 
 	if len(numberArray) == 0 {
 		return nil
@@ -55,3 +85,38 @@ func nextLineBySeparator(separator string, elementType string) interface{} {
 		return nil
 	}
 }
+
+// Q
+// エンジニアであり、社員を管理を管理する立場にあるあなたは、効率的に社員を管理するために、
+// 各社員の社員番号 number と名前 name を持ち、加えて情報を返す関数を持つような構造体、すなわち次のようなメンバ変数とメンバ関数を持つ社員クラス class employee を作成することにしました。
+
+// メンバ変数
+
+// number : 整数
+// name : 文字列
+
+// メンバ関数
+// getnum(){
+//     return number;
+// }
+// getname(){
+//     return name;
+// }
+
+// 入力で make number name と入力された場合は変数に number , name を持つ社員を作成し、 getnum n と入力された場合は n 番目に作成された社員の number を、getname n と入力された場合は n 番目に作成された社員の name を出力してください。
+
+// Input1
+// 7
+// make 2742 mako
+// getnum 1
+// make 2782 taisei
+// getname 2
+// make 31 megumi
+// getname 1
+// getname 3
+
+// Output1
+// 2742
+// taisei
+// mako
+// megumi
