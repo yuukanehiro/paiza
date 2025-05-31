69cc94496e093d108de4e340a19b6a8a52d4ec22
yuu kanehiro <info@lpb-studio.com>
2025-05-31 20:58:44 +0900

    Python | playGround | strip()

---
+++ python/playGround/string/strip.py
@@ -0,0 +1,5 @@
+
+# 前後の余白削除
+print((" hello world1").strip()) # Output: "hello world1"
+print((" hello world2　").strip()) # Output: "hello world2"
+print((" hello world3 yuu　").strip()) # Output: "hello world3 yuu"
