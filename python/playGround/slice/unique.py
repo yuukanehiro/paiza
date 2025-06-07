100d92b8aac1cd339ee41c12569abbd3df4a8612
yuu kanehiro <y.kanehiro@lpb-studio.com>
2025-06-07 22:51:04 +0900

    playGroud | slice | unique

---
+++ python/playGround/slice/unique.py
@@ -0,0 +1,17 @@
+
+
+items = [8,9,4,2,5,5,7,9,1]
+unique_items = set(items)
+sorted_unique_items = (sorted(unique_items))
+
+for v in sorted_unique_items:
+    print(v)
+
+# Output:
+# 1
+# 2
+# 4
+# 5
+# 7
+# 8
+# 9
