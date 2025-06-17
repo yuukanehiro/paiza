0778702a654ecad403a86f5e5d30eb2e2ada39d8
yuu kanehiro <yuu.kanehiro@zeroboard.jp>
2025-06-17 22:16:15 +0900

    Python | PlayGroud | tupleで降順、辞書順ソート

--- python/playGround/sort/tupleSort.py
+++ python/playGround/sort/tupleSort.py
@@ -9,3 +9,9 @@ countChars.sort()
 # 1つ目の要素を整数でソート
 # 2つめの要素を辞書順でソート
 print(countChars) # Output: [(1, 'c'), (2, 'a'), (2, 'b')]
+
+
+countChars2 = [(1, "b"), (2, "a"), (3, "c"), (5, "a"), (5, "b")]
+# 数値で降順, 辞書順
+countChars2.sort(key=lambda x: (-x[0], x[1])) # -x[0]としてマイナスをかけて昇順ソートをすると結果として降順ソートになる
+print(countChars2) # Output: [(5, 'a'), (5, 'b'), (3, 'c'), (2, 'a'), (1, 'b')]
