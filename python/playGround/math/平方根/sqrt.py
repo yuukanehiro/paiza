b8a0006b5a3b194776119dd2539cf407ca1a97d5
yuu kanehiro <11024212+yuukanehiro@users.noreply.github.com>
2025-06-23 21:05:16 +0900

    平方分割のバケット Python3編

---
+++ python/playGround/math/平方根/sqrt.py
@@ -0,0 +1,9 @@
+import math
+
+# 平方根
+print(math.sqrt(2)) # Output: 1.4142135623730951
+print(math.sqrt(4)) # Output: 2.0
+
+print(math.sqrt(10000)) # Output: 100.0
+# intで整数にして扱いやすく
+print(int(math.sqrt(10000))) # Output: 100
