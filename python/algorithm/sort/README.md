6a8bf0d05366bc52cc86be3fe37b57426d2befdf
yuu kanehiro <11024212+yuukanehiro@users.noreply.github.com>
2025-07-27 15:26:14 +0900

    Python | Algorithm | sort | クイックソート修正

---
+++ python/algorithm/sort/README.md
@@ -0,0 +1,9 @@
+
+
+* [基本情報技術者　ソートのアルゴリズムとは？](https://avinton.com/blog/2020/04/engineering-basics-sorting-algorithm/)
+* [15 Sorting Algorithms in 6 Minutes](https://www.youtube.com/watch?v=kPRA0W1kECg)
+* [クイックソート](https://www.youtube.com/watch?v=5UqHsN6GUtc)
+* [マージソート](https://www.youtube.com/watch?v=9pB5oZeD4gA)
+* [挿入ソート](https://www.youtube.com/watch?v=NyW0Uad3mxE)
+* [選択ソート](https://www.youtube.com/watch?v=7PjbywrX70U)
+* [バブルソート](https://www.youtube.com/watch?v=TEe0bzycD7c)
--- python/algorithm/sort/quick_クイックソート/main.py
+++ python/algorithm/sort/quick_クイックソート/main.py
@@ -12,9 +12,10 @@ def quick_sort(arr: List[int]) -> List[int]:
 
     pivot = arr[0]
     left = [x for x in arr[1:] if x < pivot]
+    center = [x for x in arr if x == pivot]
     right = [x for x in arr[1:] if x > pivot]
 
-    return quick_sort(left) + [pivot] + quick_sort(right)
+    return quick_sort(left) + center + quick_sort(right)
 
 
 # ------------------------
@@ -25,7 +26,30 @@ class TestQuickSort(unittest.TestCase):
         expected = [1, 2, 3, 4, 5, 7, 8, 9, 10, 99]
         actual = quick_sort([2, 99, 5, 1, 3, 7, 8, 4, 9, 10])
         self.assertEqual(expected, actual)
-
+    def test_quick_sort_case2(self):
+        expected = [1]
+        actual = quick_sort([1])
+        self.assertEqual(expected, actual)
+    def test_quick_sort_case3(self):
+        expected = [1, 2]
+        actual = quick_sort([1, 2])
+        self.assertEqual(expected, actual)
+    def test_quick_sort_case4(self):
+        expected = [1, 2, 3]
+        actual = quick_sort([3, 2, 1])
+        self.assertEqual(expected, actual)
+    def test_quick_sort_case5_empty(self):
+        expected = []
+        actual = quick_sort([])
+        self.assertEqual(expected, actual)
+    def test_quick_sort_case6_exists_zero(self):
+        expected = [0, 1, 2, 3, 99]
+        actual = quick_sort([99, 0, 2, 1, 3])
+        self.assertEqual(expected, actual)
+    def test_quick_sort_case7_duplicated(self):
+        expected = [1, 2, 2, 3, 4, 4, 5]
+        actual = quick_sort([5, 1, 2, 4, 4, 3, 2])
+        self.assertEqual(expected, actual)
 
 def main():
     arr = [2, 99, 5, 1, 3, 7, 8, 4, 9, 10]
