4c8a462064672b12fcaaf1bcbec1f87d3b6a9462
yuu kanehiro <y.kanehiro@lpb-studio.com>
2025-09-13 23:47:47 +0900

    Golang | PlayGroud | A tour of Go | Goroutines

---
+++ golang/go-tour-jp.appspot.com/concurrency/Goroutines/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	go say("world")
+	say("hello")
+}
+
+// Output:
+// world
+// hello
+// world
+// hello
+// hello
+// world
+// world
+// hello
+// hello
